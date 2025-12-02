541b688b31cf307d73cd69d4884b119b8a923843
TonyTheTigrez <thatonetigrezperson@gmail.com>
2025-12-02 19:04:53 +0000

    lol

--- week 13  csp/4 — Nested Lists and List Comprehensions.py
+++ week 13  csp/4 — Nested Lists and List Comprehensions.py
@@ -1,7 +1,57 @@
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+nested_list = [list1, list2]
+
+print(nested_list[0])
+
+print(nested_list[1][1])
+
+print(nested_list[1][0])
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Objective:
 # Students will manipulate nested lists and understand basic list comprehensions.
 
 # Key Notes:
+fruits = ["apple", "orange", "bannana", "coconut"]
+vege = ["celery", "carrots", "potatoes"]
+meats = ["chicken", "fish", "turkey"]
+
+groceries = [fruits, vege, meats]
+
+print(groceries[0][2])
+
+num_pad = ((1,2,3),
+           (4,5,6),
+           (7,8,9),
+           ("*", 0, "#"))
+
+for row in num_pad:
+    for num in row:
+        print(num, end=" ")
+    print()
+
+
+
+
+           
+           
+
+
+
+
 
 # A list can contain other lists.
 
@@ -30,7 +80,11 @@ print(matrix[1][2])    # 6
 first_col = [row[0] for row in matrix]
 print(first_col)       # [1, 4, 7]
 
+sum_list = [row[-1] for row in matrix]
+print(sum_list)
 
+squares = [x**2 for x in range(1, 11)]
+print(squares)
 
 # Practice Problems:
 
---
+++ week 13  csp/sets_tuples.py
@@ -0,0 +1,22 @@
+
+set1 = {1, 2, 3, 4, 5}
+print(set1)
+print(type(set1))
+set1.add(6)
+print(set1) # 1, 2, 3, 4, 5, 6
+set1.remove(2)
+print(set1)
+
+#sets drop duplicate stuff
+set2 = {"apple", "banana", "cherry", "cherry"}
+print(set2)
+tuple1 = (1, 2, 3, 4, 5)
+print(tuple1)
+
+print(type(tuple1))
+# tuples are immutable, cannot be changed
+# makes tuples useful for storing data that cannot be moved
+
+ssn = (123456, 444456, 123871)
+
+print(ssn)
