5676f11eaf6e2c6644b3558b8774fe6d9737da4e
TonyTheTigrez <thatonetigrezperson@gmail.com>
2025-12-02 19:19:56 +0000

    lol

--- week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
+++ week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
@@ -12,3 +12,13 @@
 
 # Starter Code:
 
+tempature = int(input("What is today's tempature?: "))
+
+if tempature >= -10 and tempature <= 45:
+    print("It is cold out!")
+elif tempature > 45 and tempature < 75:
+    print("It is warm out!")
+elif tempature <= 75 and tempature <= 110:
+    print("It is hot out!")
+else:
+    print("Extreme tempature warning!")
